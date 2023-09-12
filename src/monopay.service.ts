4965592be645299ee01bc22d1152edaadc618466
kasraghoreyshi <kasra.ghoreyshi84@gmail.com>
2023-09-13 02:25:09 +0330

    Fix driverName

--- src/monopay.service.ts
+++ src/monopay.service.ts
@@ -1,29 +1,31 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { getPaymentDriver } from 'monopay';
-import { Driver } from 'monopay/dist/driver';
-import { MONOPAY_CONFIG } from './monopay.constants';
+import { Inject, Injectable } from "@nestjs/common";
+import { DriverName, getPaymentDriver } from "monopay";
+import { Driver } from "monopay/dist/driver";
+import { MONOPAY, MONOPAY_CONFIG } from "./monopay.constants";
 @Injectable()
 export class MonopayService<T extends Driver> {
   private readonly driver: Driver;
 
   constructor(
+    @Inject(MONOPAY)
+    private readonly driverName: DriverName,
     @Inject(MONOPAY_CONFIG)
-    private readonly config: Parameters<T['setConfig']>[0],
+    private readonly config: Parameters<T["setConfig"]>[0]
   ) {
-    this.driver = getPaymentDriver<T>('zarinpal', this.config);
+    this.driver = getPaymentDriver<T>(this.driverName, this.config);
   }
 
-  async requestPayment(options: Parameters<T['requestPayment']>[0]) {
+  async requestPayment(options: Parameters<T["requestPayment"]>[0]) {
     return this.driver.requestPayment(options);
   }
 
-  setConfig(options: Parameters<T['setConfig']>[0]) {
+  setConfig(options: Parameters<T["setConfig"]>[0]) {
     return this.driver.setConfig(options);
   }
 
   async verifyPayment(
-    options: Parameters<T['verifyPayment']>[0],
-    params: Parameters<T['verifyPayment']>[1],
+    options: Parameters<T["verifyPayment"]>[0],
+    params: Parameters<T["verifyPayment"]>[1]
   ) {
     return this.driver.verifyPayment(options, params);
   }
